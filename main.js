38d74145a9c20d4149342142f68f78a55a4ecc7f
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-04-27 18:49:28 +0700

    feat: Add getComputerChoice function

---
+++ main.js
@@ -0,0 +1,8 @@
+// returnn rock, paper, scissor
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) return 'rock';
+    if (randomNumber === 1) return 'paper';
+    if (randomNumber === 2) return 'scissor';
+}
+
