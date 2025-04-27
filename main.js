84a986cbff55001bcaf7f7bc3e0becab6e77cd67
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-04-27 19:19:04 +0700

    feat(core): Add ability to play round in cli

--- main.js
+++ main.js
@@ -10,3 +10,56 @@ function getHumanChoice() {
     const result = window.prompt(`Choose between 'rock', 'paper', or 'scissor'`);
     return result;
 }
+
+// output: 0 mean tie, 1 mean user win, 2 mean compute win.
+function getWinner(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (humanChoice === 'rock') {
+        if (computerChoice === 'rock') return 0;
+        if (computerChoice === 'paper') return 2;
+        if (computerChoice === 'scissor') return 1;
+    }
+
+    if (humanChoice === 'paper') {
+        if (computerChoice === 'rock') return 1;
+        if (computerChoice === 'paper') return 0;
+        if (computerChoice === 'scissor') return 2;
+    }
+
+    if (humanChoice === 'scissor') {
+        if (computerChoice === 'rock') return 2;
+        if (computerChoice === 'paper') return 1;
+        if (computerChoice === 'scissor') return 0;
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRoundCLI() {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(humanChoice, computerChoice);
+
+    if (winner === 0) {
+        console.log('Tie');
+        console.log(`Human Score: ${humanScore}`)
+        console.log(`Computer Score: ${computerScore}`)
+    }
+
+    // human win
+    if (winner === 1) {
+        console.log('You win');
+        console.log(`Human Score: ${++humanScore}`)
+        console.log(`Computer Score: ${computerScore}`)
+    }
+
+    // Computer win
+    if (winner === 2) {
+        console.log('Computer win');
+        console.log(`Human Score: ${humanScore}`)
+        console.log(`Computer Score: ${++computerScore}`)
+    }
+}
